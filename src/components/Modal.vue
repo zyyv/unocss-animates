83fa4752c8f6bbd7dcd28ac9fb3e1ac95395276b
Chris <hizyyv@gmail.com>
2023-12-20 16:00:15 +0800

    chore: update

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -20,6 +20,17 @@ const keyframeCSS = computedAsync(async () => {
 
 const { copy } = useClipboard()
 
+function handleCopy(value: string) {
+  try {
+    copy(value)
+    alert('Copied!')
+  }
+  catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
+  }
+}
+
 watchEffect(()=>{
   if (visible.value) 
     document.body.style.overflow = 'hidden'
@@ -34,44 +45,44 @@ watchEffect(()=>{
     pf
     w-screen
     h-screen
+    fcc
     bg="#222 op-80"
     z-10
   >
-    <div w-full h-full fcc>
-      <div pr fccc gap-1 class="prose">
-        <div
-          text-xl
-          pa
-          top-0
-          right-0
-          i-ri:close-circle-line
-          hover-i-ri:close-circle-fill
-          cursor-pointer
-          @click="visible = false"
-        />
-        <h2
-          mt-0
-          fw-600
-          text-3xl
-          mb-2
-          px-4
-          py-2
-          text-red
-          hover="b b-red-300 b-dashed rd-2 cursor-pointer"
-          @click="copy(animateInfo.name)"
-        >
-          {{ animateInfo.name }}
-        </h2>
-        <p>
-          Class: <span
-            hover="b b-teal-300 b-dashed rd-2 cursor-pointer"
-            inline-block
-            p="x-2"
-            text-teal
-            @click="copy(`animate-${animateInfo.name}`)"
-          >animate-{{ animateInfo.name }}</span>
-        </p>
-        <!-- <p>
+    <div pr fccc gap-1 class="prose">
+      <div
+        text-xl
+        pa
+        top-0
+        right-0
+        i-ri:close-circle-line
+        hover-i-ri:close-circle-fill
+        cursor-pointer
+        @click="visible = false"
+      />
+      <h2
+        mt-0
+        fw-600
+        text-3xl
+        mb-2
+        px-4
+        py-2
+        text-red
+        hover="b b-red-300 b-dashed rd-2 cursor-pointer"
+        @click="handleCopy(animateInfo.name)"
+      >
+        {{ animateInfo.name }}
+      </h2>
+      <p>
+        Uno Class: <span
+          hover="b b-teal-300 b-dashed rd-2 cursor-pointer"
+          inline-block
+          p="x-2"
+          text-teal
+          @click="handleCopy(`animate-${animateInfo.name}`)"
+        >animate-{{ animateInfo.name }}</span>
+      </p>
+      <!-- <p>
           <label fw-500 text="teal sm right" inline-block w-30>Count:</label>
           {{ animateInfo.count }}
         </p>
@@ -87,20 +98,19 @@ watchEffect(()=>{
           <label fw-500 text="teal sm right" inline-block w-30>Property:</label>
           {{ animateInfo.property }}
         </p> -->
-        <!-- <p>
+      <!-- <p>
           <label fw-500 text="teal sm right" inline-block w-30>Keyframe:</label> -->
-        <div max-w-screen pr>
-          <div
-            pa
-            right-4
-            top-8
-            i-ri:clipboard-line
-            hover-i-ri:clipboard-fill
-            cursor-pointer
-            @click="copy(fotmatkeyframeCSS)"
-          />
-          <div v-html="keyframeCSS" />
-        </div>
+      <div max-w-screen pr>
+        <div
+          pa
+          right-4
+          top-8
+          i-ri:clipboard-line
+          hover-i-ri:clipboard-fill
+          cursor-pointer
+          @click="handleCopy(fotmatkeyframeCSS)"
+        />
+        <div v-html="keyframeCSS" />
       </div>
     </div>
   </div>
