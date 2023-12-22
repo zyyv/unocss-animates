df73780a79d1590219c900a2e0e4c4281fe7f490
Chris <hizyyv@gmail.com>
2023-12-22 17:17:07 +0800

    refactor: use shikiji

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,6 @@
-<script setup lang="ts">
-import { getHighlighter } from 'shiki-es'
-import type { Highlighter } from 'shiki-es'
-
-const highlighter = ref<Highlighter>()
-
-async function useHighlighter() {
-  highlighter.value = await getHighlighter({ theme: 'vitesse-dark' })
-}
-
-onBeforeMount(useHighlighter)
-</script>
-
 <template>
   <div of-x-hidden>
-    <Modal :highlighter="highlighter" />
+    <Modal />
     <TheHeader />
     <Preview />
   </div>
--- src/components/Animate.vue
+++ src/components/Animate.vue
@@ -7,9 +7,7 @@ defineProps<{
 
 <template>
   <div
-
     aspect="1/1"
-
     transtion w-full fcc b rd-2 hover-cursor-pointer hover-b-purple-400 hover-text-purple-400
     :class="!status && 'group'"
   >
