a3475b809be6fbb1626459973e3eac6084dc5d82
Chris <hizyyv@gmail.com>
2023-12-23 23:24:26 +0800

    feat: header optimize

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,57 @@
 <template>
-  <header
+  <header h-10 w-full fbc px-4 md-h-18>
+    <div pr w-14 h-14 fcc>
+      <div animate-pulse pa top-0 left-0 w-full h-full rd-full blur-lg z-1 style="background-image: linear-gradient( -45deg, #44444460 30%, #88888860 );"></div>
+      <div i-logos-unocss animate-duration-2000 animate-count-infinite text-3xl />
+    </div>
+    <h1 text="#bbb" fcc gap-2>
+      <div fcc class="animate-count-infinite" :class="{ 'children-animate-none': !isOpenAnimate }">
+        <div animate-light-speed-in-left>
+          U
+        </div>
+        <div animate-rubber-band>
+          n
+        </div>
+        <div animate-bounce>
+          o
+        </div>
+        <div animate-heart-beat>
+          C
+        </div>
+        <div animate-boingInUp>
+          S
+        </div>
+        <div animate-light-speed-in-right>
+          S
+        </div>
+      </div>
 
-    h-10 w-full fbc px-4 md-h-18
-  >
-    <div />
-    <h1 text="#bbb">
-      UnoCSS Animations
+      <div fcc class="animate-count-infinite" :class="{ 'children-animate-none': !isOpenAnimate }">
+        <div animate-light-speed-in-left>
+          A
+        </div>
+        <div animate-rubber-band>
+          n
+        </div>
+        <div animate-bounce>
+          i
+        </div>
+        <div animate-heart-beat>
+          m
+        </div>
+        <div animate-boingInUp>
+          a
+        </div>
+        <div animate-light-speed-in-right>
+          t
+        </div>
+        <div animate-slideLeft>
+          e
+        </div>
+        <div animate-rubber-band>
+          s
+        </div>
+      </div>
     </h1>
     <div text-md fsc gap-2 md:text-xl>
       <div cursor-pointer title="Toggle the animations" @click="toggleIsOpenAnimate()">
