6585fabb5e490659c9ad2121f42d2a61738b84fd
Chris <hizyyv@gmail.com>
2023-12-26 23:33:50 +0800

    chore: update

--- src/composables/data.ts
+++ src/composables/data.ts
@@ -1,6 +1,4 @@
-import type { Theme } from '@unocss/preset-mini'
-
-export const All_Animations = ref((import.meta.env.__UNO_THEME__ as Theme).animation)
+export const All_Animations = ref(import.meta.env.__UNO_THEME__.animation)
 
 export function hasDuration(name: string) {
   return !!All_Animations.value?.durations?.[name]
