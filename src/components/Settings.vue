0b0755d64e8504073e247c836cb31545dffabb3b
Chris <hizyyv@gmail.com>
2023-12-26 10:17:55 +0800

    chore: update

---
+++ src/components/Settings.vue
@@ -0,0 +1,14 @@
+<script lang='ts' setup>
+import { defineProps } from 'vue'
+</script>
+
+<template>
+  <div w-screen h-screen pf top-0 left-0 bg="black op-20" fe>
+    <div w="1/2" h-full backdrop-blur-4>
+      11
+    </div>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+</style>
